6c95e0e9c19f134215edee7f62381455631b3950
davemuir <davidmuirdesign@gmail.com>
2012-12-05 17:12:31 -0500

    trying to match up

--- wp-content/themes/repro/index copy.php
+++ wp-content/themes/repro/index copy.php
@@ -50,6 +50,79 @@ $tz_bottom_blocks = get_option('tz_bottom_blocks');
        			 <div class="clear"></div>
         	</div><!--end middle-->
         
+
+        	
+        	
+            <?php if($tz_slider_display == 'true') : ?>
+            
+            <?php get_template_part('index', 'slider'); ?>
+            
+            <?php endif; ?>
+            
+            
+            
+            <?php if($tz_post_grid_display == 'true') : ?>
+            
+            <div class="grid_8 alpha omega">
+            
+                <?php get_template_part('index', 'post-grid'); ?>
+            
+            </div><!--grid_8 alpha omega-->
+            
+            <?php endif; ?>
+            
+            <?php if($tz_top_blocks == 'true') : ?>
+            
+            <div class="grid_8 alpha omega">
+            
+                <div class="grid_4 alpha">
+            
+                    <?php get_template_part('index', 'category-list-one'); ?>
+                    
+                </div><!--grid_4-->
+                
+                <div class="grid_4 omega">
+                
+                    <?php get_template_part('index', 'category-list-two'); ?>
+                    
+                </div><!--grid_4-->
+            
+            </div><!-- grid_8 alpha omega-->
+            
+            <?php endif; ?>
+            
+            <?php if($tz_bottom_blocks == 'true') : ?>
+            
+            <div class="grid_8 alpha omega">
+            
+                <div class="grid_4 alpha">
+            
+                    <?php get_template_part('index', 'category-list-three'); ?>
+                    
+                </div><!--grid_4-->
+                
+                <div class="grid_4 omega">
+                
+                    <?php get_template_part('index', 'category-list-four'); ?>
+                    
+                </div><!--grid_4-->
+            
+            </div><!-- grid_8 alpha omega-->
+            
+            <?php endif; ?>
+    
+        </div><!--grid_8-->
+        
+      
+        
+       <div class="grid_4" style="margin-right: 0;">
+        
+            <?php get_sidebar(); ?>
+          
+        </div>
+
+        <div class="clear"></div>
+        
     </div><!--container_12-->
     	
     

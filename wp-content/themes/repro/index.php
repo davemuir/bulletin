e538a1c47ddd12fe6fe1411615d4978ca0095172
davemuir <davidmuirdesign@gmail.com>
2012-12-06 17:16:57 -0500

    making loops

--- wp-content/themes/repro/index.php
+++ wp-content/themes/repro/index.php
@@ -18,8 +18,44 @@ $tz_bottom_blocks = get_option('tz_bottom_blocks');
 			<!--includes slider-->
 			<div class="grid_8">
 				<div id="top_news">
-					<p>story</p>
-					<?php the_title()?>
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					
+						 <?php if ( in_category('1') ) { ?>
+								   <div id="top_news">
+						 <?php } else { ?>
+								   <div class="post">
+						 <?php } ?>
+						
+						
+						 <!-- Display the Title as a link to the Post's permalink. -->
+						
+						 <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+						
+						
+						 <!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
+						
+						 <small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
+						
+						
+						 <!-- Display the Post's content in a div box. -->
+						
+						 <div class="entry">
+						   <?php the_content(); ?>
+						 </div>
+						
+						
+						 <!-- Display a comma separated list of the Post's Categories. -->
+						
+						 <p class="postmetadata">Posted in <?php the_category(', '); ?></p>
+						 </div> <!-- closes the first div box -->
+						
+						
+						 <!-- Stop The Loop (but note the "else:" - see next line). -->
+					
+					<?php endwhile; else: ?>
+						<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+					<?php endif; ?>
+
 				</div>
 			</div><!--end grid8-->	
 			<div class="new_grid_4">

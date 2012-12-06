d6bdf6b8918dacba13b748ea9c622d1eedb2c04d
davemuir <davidmuirdesign@gmail.com>
2012-12-06 18:12:55 -0500

    the_content and more

--- wp-content/themes/repro/index.php
+++ wp-content/themes/repro/index.php
@@ -23,8 +23,11 @@ $tz_bottom_blocks = get_option('tz_bottom_blocks');
 						<?php $my_query = new WP_Query('category_name=latest&posts_per_page=1');
 						  while ($my_query->have_posts()) : $my_query->the_post();
 						  $do_not_duplicate = $post->ID; ?>
-						  
-						  <?php the_content(); ?>
+						  <h2><?php the_title() ?></h2>
+						  <h6><?php the_time(get_option('date_format')) ?>,<?php the_category() ?></h6>
+						  <div id="latest_content">
+						 	 <p><?php the_content(); ?></p>
+						 </div>
 						<?php endwhile; ?>
 
 					

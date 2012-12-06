7afd19e0cc15e6604577ebd1714c2d7d45662cc2
davemuir <davidmuirdesign@gmail.com>
2012-12-06 05:42:25 -0500

    changing out a h4

--- wp-content/themes/repro/index.php
+++ wp-content/themes/repro/index.php
@@ -33,11 +33,11 @@ $tz_bottom_blocks = get_option('tz_bottom_blocks');
     </div><!--container_12-->
     
     <div class="container_12">
-        
+        <h4 class="hfour"><span> Toronto Scene </span></h4>
         <div id="the_middle">
 			<!--includes slider-->
 			<div class="grid_8">
-				<h4 class="hfour"><span> Toronto Scene </span></h4>
+				
 				<div id="galleria">
 					<!--make looping photo addition-->
 					

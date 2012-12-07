db2f03fbde57394315f6b553de94b5b4611badc1
davemuir <davidmuirdesign@gmail.com>
2012-12-07 18:21:00 -0500

    reformatting one more maskma

--- wp-content/themes/repro/index.php
+++ wp-content/themes/repro/index.php
@@ -33,28 +33,7 @@ $tz_bottom_blocks = get_option('tz_bottom_blocks');
 							 <?php the_post_thumbnail(array(300,228)) ?>
 						</div>
 						<div id="latest_excerpt">
-						 	<?php
-the_excerpt_max_charlength(140);
-
-function the_excerpt_max_charlength($charlength) {
-	$excerpt = get_the_excerpt();
-	$charlength++;
-
-	if ( mb_strlen( $excerpt ) > $charlength ) {
-		$subex = mb_substr( $excerpt, 0, $charlength - 5 );
-		$exwords = explode( ' ', $subex );
-		$excut = - ( mb_strlen( $exwords[ count( $exwords ) - 1 ] ) );
-		if ( $excut < 0 ) {
-			echo mb_substr( $subex, 0, $excut );
-		} else {
-			echo $subex;
-		}
-		echo '[...]';
-	} else {
-		echo $excerpt;
-	}
-}
-?>
+						 	<?php custom_excerpt_length(); ?>
 						</div> 	
 						<?php endwhile; ?>
 

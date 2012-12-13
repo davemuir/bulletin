890f92f532854e20e2c50a4308e6508babbfb9d2
davemuir <davidmuirdesign@gmail.com>
2012-12-13 18:40:13 -0500

    redoing old shit

--- wp-content/themes/repro/index.php
+++ wp-content/themes/repro/index.php
@@ -124,30 +124,19 @@ $tz_recent_number = get_option('tz_recent_number');
 						  $do_not_duplicate = $post->ID; ?>
 						  
 						  <div class="column">
-    	
-									<?php if (  (function_exists('has_post_thumbnail')) && (has_post_thumbnail())  ) : /* if post has post thumbnail */ ?>
-									
-									<div class="image"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('column-preview'); ?></a></div>
-									
-									<?php endif; ?>
-									
-									<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-									
-									<div class="date"><?php the_time( get_option('date_format') ); ?>,<br/><?php the_category('$seperator');?>, <?php comments_popup_link(__('No comments', 'framework'), __('1 Comment', 'framework'), __('% Comments', 'framework')); ?></div>
-									
-									<div class="excerpt">
-										<p><?php echo substr(get_the_excerpt(), 0, 150); ?>
-										   <?php if(strlen(get_the_excerpt()) > 150) : ?>...<?php endif; ?> </p>
-									</div><!--excerpt-->
-								
-							</div><!--column-->
-								
-								<div class="clear"></div>
-							
-					</div><!--post_columns-->
-					
+							  <h1><?php the_title() ?></h1>
+							  <h6><?php the_time(get_option('date_format')) ?></h6>
+						
+							 <?php if (  (function_exists('has_post_thumbnail')) && (has_post_thumbnail())  ) : /* if post has post thumbnail */ ?>
+        						<div class="image"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('column-preview'); ?></a></div>
+        					<?php endif; ?>
+							<?php the_excerpt()?>
+						 
+						 
+						</div> 	
+					<?php endwhile; ?>
                 
-           <!--grid_8 alpha omega or news8Col-->
+            </div><!--grid_8 alpha omega or news8Col-->
     	
     	
     	<div class="clear"></div>

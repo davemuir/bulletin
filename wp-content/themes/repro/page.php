cab54a95ac98cbc52c3da2b80ee70c9b1ccb3659
davemuir <davidmuirdesign@gmail.com>
2013-01-02 15:02:33 -0500

    more social media and re size images

--- wp-content/themes/repro/page.php
+++ wp-content/themes/repro/page.php
@@ -59,7 +59,16 @@ $tz_image_display = get_option('tz_image_display');
             </div><!--grid_8-->
             
             <div class="grid_4">
-            
+            	<div id="socialIcons">
+				<div style="float:left;width:150px;height:36px;">
+					<div class="fb-like" data-href="http://www.facebook.com/pages/The-Bulletin-Record-Zambia/153672558013677?ref=ts&amp;fref=ts" data-send="true" data-layout="button_count" data-width="200" data-show-faces="false" data-font="arial"></div>		
+				</div>
+				<div style="float:left;width:150px;height:36px;">
+					    <a href="https://twitter.com/TheBulletinca" class="twitter-follow-button" data-show-screen-name="false" data-show-count="true" show_screen_name="true" data-lang="en">Follow @thebulletinca</a>
+
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+				</div>
+				</div>
 				<?php get_sidebar(); ?>
               
             </div><!--grid_4-->

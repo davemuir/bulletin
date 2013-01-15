7ad2e6b88d26206b5542d39221b6831382c9d865
davemuir <davidmuirdesign@gmail.com>
2013-01-15 12:22:54 -0500

    adding in the header witha banner

--- wp-content/themes/repro/header.php
+++ wp-content/themes/repro/header.php
@@ -2,10 +2,8 @@
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <!-- A ThemeZilla design (http://www.themezilla.com) - Proudly powered by WordPress (http://wordpress.org) -->
-
 <!-- BEGIN head -->
 <head>
-
 	<!-- Meta Tags -->
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 	
@@ -24,12 +22,10 @@
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	
 	<?php wp_head(); ?>
-
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
 	<script src="./wp-content/themes/repro/js/galleria-1.2.8.min.js"></script>
 <!-- END head -->
 </head>
-
 <!-- BEGIN body -->
 <body <?php body_class(); ?>>
 	<div id="fb-root"></div>
@@ -40,7 +36,6 @@
   js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=323978084354873";
   fjs.parentNode.insertBefore(js, fjs);jpeg
 }(document, 'script', 'facebook-jssdk'));</script>
-
 <div id="header">
         
     <div id="top">
@@ -59,7 +54,6 @@
             <div class="grid_3">
                   
                 <div id="rss">
-
                 	<span><?php _e('Subscribe', 'framework') ?></span> 
 					<?php _e('By', 'framework') ?> 
 					
@@ -80,6 +74,8 @@
             </div><!--grid_3-->
             
             <?php endif; ?>
+            
+            
             
             <div class="grid_9 right">
             
@@ -112,7 +108,6 @@
             </div><!--grid_9-->
             
             <div class="clear"></div>
-
         </div><!--container_12-->
     
     </div><!--top-->
@@ -157,6 +152,64 @@
               </div><!--logo-->
               
             </div><!--grid_5-->
+			<div id="topAdRotater">	
+<!-- Begin DWUser_EasyRotator -->
+<script type="text/javascript" src="http://c520866.r66.cf2.rackcdn.com/1/js/easy_rotator.min.js"></script>
+<div class="dwuserEasyRotator" style="width: 468px; height: 60px; position:relative; text-align: left;" data-erConfig="{autoplayEnabled:false, lpp:'102-105-108-101-58-47-47-47-85-115-101-114-115-47-49-47-68-111-99-117-109-101-110-116-115-47-69-97-115-121-82-111-116-97-116-111-114-80-114-101-118-105-101-119-47-112-114-101-118-105-101-119-95-115-119-102-115-47', wv:1}" data-erName="topAdRotater">
+	<div data-ertype="content" style="display: none;"><ul data-erlabel="top ads">
+	<li>
+		<img class="main" src="tscene/TorontoEatonCentreGeese_CourtesyTorontoTourism.jpeg" />
+		<img class="thumb" src="tscene/TorontoEatonCentreGeese_CourtesyTorontoTourism.jpeg" />
+	</li>
+</ul>
+</div>
+	<div data-ertype="layout" data-ertemplateName="NONE" style="">
+		
+		<div class="erimgMain" style="position: absolute; left:0;right:0;top:0;bottom:70px;" data-erConfig="{___numTiles:3, scaleMode:'fillArea', imgType:'main', __loopNextButton:false, arrowButtonMode:'rollover'}">
+			<div class="erimgMain_slides" style="position: absolute; left:0px; top:0; bottom:0; right:0px;">
+				<div class="erimgMain_slide">
+					<div class="erimgMain_img" style="position: absolute; left: 0; right: 0; top: 0; bottom: 0;"></div>
+					<div class="erhideWhenNoText" style="background: #000; background: rgba(0,0,0,0.85); position: absolute; left: 0; right: 0; bottom: 0; padding: 5px; color: #FFF; font-family: Arial; font-size: 12px;">
+						<p class="erimgMain_title" style="padding: 0; margin: 0 0 3px 0; font-weight: bold;"></p>
+						<p class="erimgMain_desc" style="padding: 0 0 10px 0; margin: 0;"></p>
+					</div>
+				</div>
+			</div>
+			<div class="erimgMain_arrowLeft" style="position:absolute; left: 10px; top: 50%; margin-top: -15px;" data-erConfig="{image:'circleSmall', image2:'circleSmall'}"></div>
+			<div class="erimgMain_arrowRight" style="position:absolute; right: 10px; top: 50%; margin-top: -15px;"></div>
+		</div>
+		<div class="erimgMain rotatorTileNav" style="position: absolute; left:0;right:0;bottom:0;height:80px;" data-erConfig="{numTiles:-1, scaleMode:'fillArea', imgType:'thumb', loopNextButton:false, arrowButtonMode:'rollover', __slideLinkEvent:'rollover'}">
+			<div style="position: absolute; left: 0; top: 10px; right: 0; bottom: 0; background: #FFF;"></div>
+			<div class="erimgMain_slides" style="position: absolute; left:0px; top:0; bottom:0; right:0px;">
+				<div class="erimgMain_slide">
+					<div class="erimgMain_img" style="position: absolute; left: 0; right: 0; top: 10px; bottom: 0; margin: 2px 1px;"></div>
+					<!-- <div class="" style="background: #555; position: absolute; left: 1px; right: 1px; top: 10px; bottom: 0; padding: 5px; color: #FFF; font-family: Arial; font-size: 12px; text-align: center;">
+						<p class="erimgMain_title" style="padding: 5px; margin: 0 0 3px 0; font-weight: bold;"></p>
+					</div> -->
+					<div class="selectionArrow visibleWhenSelected" style="position: absolute; top: 0; left: 50%; margin-left: -10px; width: 20px; height: 10px; background-image: url('http://easyrotator.s3.amazonaws.com/1/i/rotator/FFF_arrow10_export.png');"></div>
+				</div>
+			</div>
+			<div class="erimgMain_arrowLeft" style="position:absolute; left: 60px; top: 50%; margin-top: -10px;" data-erConfig="{image:'circleSmall', image2:'circleSmall'}"></div>
+			<div class="erimgMain_arrowRight" style="position:absolute; right: 60px; top: 50%; margin-top: -10px;"></div>
+		</div>
+				
+		
+		<div class="erabout erFixCSS3" style="color: #FFF; text-align: left; background: #000; background:rgba(0,0,0,0.93); border: 2px solid #FFF; padding: 20px; font: normal 11px/14px Verdana,_sans; width: 300px; border-radius: 10px; display:none;">
+			This <a style="color:#FFF;" href="http://www.dwuser.com/easyrotator/" target="_blank">jQuery slider</a> was created with the free <a style="color:#FFF;" href="http://www.dwuser.com/easyrotator/" target="_blank">EasyRotator</a> software from DWUser.com.
+			<br /><br />
+            Use WordPress? The free <a style="color:#FFF;" href="http://www.dwuser.com/easyrotator/wordpress/" target="_blank">EasyRotator for WordPress</a> plugin lets you create beautiful <a style="color:#FFF;" href="http://www.dwuser.com/easyrotator/wordpress/" target="_blank">WordPress sliders</a> in seconds.
+			<br /><br />
+			<a style="color:#FFF;" href="#" class="erabout_ok">OK</a>   
+		</div>
+		<noscript>
+			Rotator powered by <a href="http://www.dwuser.com/easyrotator/">EasyRotator</a>, a free and easy jQuery slider builder from DWUser.com.  Please enable JavaScript to view.
+		</noscript>
+		<script type="text/javascript">/*Avoid IE gzip bug*/(function(b,c,d){try{if(!b[d]){b[d]="temp";var a=c.createElement("script");a.type="text/javascript";a.src="http://easyrotator.s3.amazonaws.com/1/js/nozip/easy_rotator.min.js";c.getElementsByTagName("head")[0].appendChild(a)}}catch(e){alert("EasyRotator fail; contact support.")}})(window,document,"er_$144");</script>
+	
+	</div>
+</div>
+<!-- End DWUser_EasyRotator -->
+			</div>
 			
             <?php if ($tz_banner_header == "true") : /* Display 468x60 banner if checked in theme options */ ?>
             
